046583c6c27a3d3d7a70c0baf559818382ebef1c
Paul <pawelpelar@gmail.com>
2023-05-18 10:57:27 +0200

    Sim annealing

--- sim_ann.py
+++ sim_ann.py
@@ -1,5 +1,8 @@
 import numpy as np
 import random
+from cities_gen import generate_distance_matrix
+import random as rd
+
 
 def simulated_annealing(n_cities, iterations, initial_temperature, cooling_rate):
     distance_matrix = generate_distance_matrix(n_cities)
@@ -43,7 +46,23 @@ def accept_solution(current_distance, new_distance, temperature):
         return random.random() < acceptance_probability
 
 
-# Rest of the code remains the same
+def neighbour_switch(solution):
+    i, j = 0, 0
+    while i == j:
+        i, j = rd.randint(0, len(solution) - 1), rd.randint(0, len(solution) - 1)
+    solution[i], solution[j] = solution[j], solution[i]
+    return solution
+
+
+def calc_distance(distance_matrix, solution):
+    N = distance_matrix.shape[0]
+    total_distance = 0
+    for i in range(N-1):
+        total_distance += distance_matrix[solution[i], solution[i+1]]
+    total_distance += distance_matrix[solution[N-1], solution[0]]
+
+    return total_distance
+
 
 if __name__ == "__main__":
     n_cities = 5
@@ -51,7 +70,8 @@ if __name__ == "__main__":
     initial_temperature = 100.0
     cooling_rate = 0.95
 
-    best_solution, best_distance = simulated_annealing(n_cities, iterations, initial_temperature, cooling_rate)
+    best_solution, best_distance = simulated_annealing(
+        n_cities, iterations, initial_temperature, cooling_rate)
 
     print("Best solution:", best_solution)
     print("Best distance:", best_distance)
